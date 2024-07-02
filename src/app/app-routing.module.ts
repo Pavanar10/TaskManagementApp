7d2413fd254442ab9161833096d19aa6d189dc3f
Pavanar10 <pavanar10@gmail.com>
2024-07-02 12:54:44 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoardsComponent } from './pages/boards/boards.component';
+import { ViewBoardComponent } from './pages/view-board/view-board.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'boards',component:BoardsComponent
+},{
+  path:'',redirectTo:'boards',pathMatch:'full'
+},{
+  path:'viewboard/:boardIndex',component:ViewBoardComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { BoardsComponent } from './pages/boards/boards.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import { NewBoardComponent } from './components/new-board/new-board.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { ViewBoardComponent } from './pages/view-board/view-board.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { NewcardComponent } from './components/newcard/newcard.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    BoardsComponent,
+    NewBoardComponent,
+    ViewBoardComponent,
+    NewcardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
